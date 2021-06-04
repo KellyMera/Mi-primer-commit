9ad04d6e8ab5b8d9b9035696c67465f02595ec1b
KellyMera <milenamera01@gmail.com>
2021-06-03 19:41:58 -0500

    Mi primer commit

---
+++ Python/ejercicio001.py
@@ -0,0 +1,15 @@
+# declaraciones de variables
+nombre = "Kelly Milena Mera Córdova"
+edad = 19
+genero = 'F'
+telefono = '0982109453'
+# maneras de impresión
+print (nombre)
+print (type(nombre))
+print ("Mi nombre es: " + nombre + ", edad: " + str(edad)+ ", genero: " +genero+ " y mi telefono es: " +telefono)
+print (f"Mi nombre es: {nombre}, edad: {edad}, genero: {genero}, telefono: {telefono}")
+
+primer_numero = float (input ("Por favor, ingrese el primer número: ")) 
+segundo_numero = float (input ("Por favor, ingrese el segundo número: ")) 
+resultado = primer_numero + segundo_numero
+print (resultado) 
---
+++ Python/ejercicio002.py
@@ -0,0 +1,12 @@
+# Calculo del area y perímetro de un cuadrado
+lado = float (input ("Por favor, ingrese la medida del lado del cuadrado: ")) 
+
+# Operaciones
+area = lado * lado
+perimetro = lado * 4
+
+# Impresión de los resultados
+print ("El área del cuadrado es: ") 
+print (area)
+print ("El perímetro del cuadrado es: ") 
+print (perimetro)
