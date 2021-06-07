eaec18913521b4ba712d0397b07c93974de3c6b7
KellyMera <milenamera01@gmail.com>
2021-06-07 15:35:38 -0500

    Este es mi primer commit

---
+++ Python/ejercicio003.py
@@ -0,0 +1,7 @@
+#Ejercicio de área y perímetro de un triángulo
+base = float (input("Ingrese la base del triánngulo: "))
+altura = float (input("Ingresa la altura del triángulo: "))
+
+#Operaciones
+print (f"El área del triángulo es: {(base*altura)/2}")
+print (f"El perímetro del triángulo es: {base*3}")
---
+++ Python/ejercicio004.py
@@ -0,0 +1,8 @@
+#Ejercicio del área y perímetro del círculo
+radio = float (input("Ingrese el radio del círculo: "))
+pi = 3.1415
+
+#Operaciones
+radio2 = radio * radio 
+print (f"El área del círculo es: {pi * radio2}")
+print (f"El perímetro del círculo es: {2 * pi * radio}")
