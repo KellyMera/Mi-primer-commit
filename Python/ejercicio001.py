7f36ec757763f9ba8b4e5eecf5361597e72ae2cb
KellyMera <milenamera01@gmail.com>
2021-06-07 18:42:11 -0500

    Mi primer commit

--- Python/ejercicio001.py
+++ Python/ejercicio001.py
@@ -1,15 +1,17 @@
-# declaraciones de variables
+# Declaraciones de variables
 nombre = "Kelly Milena Mera Córdova"
 edad = 19
 genero = 'F'
 telefono = '0982109453'
-# maneras de impresión
+
+# Maneras de impresión
 print (nombre)
 print (type(nombre))
 print ("Mi nombre es: " + nombre + ", edad: " + str(edad)+ ", genero: " +genero+ " y mi telefono es: " +telefono)
 print (f"Mi nombre es: {nombre}, edad: {edad}, genero: {genero}, telefono: {telefono}")
 
-primer_numero = float (input ("Por favor, ingrese el primer número: ")) 
-segundo_numero = float (input ("Por favor, ingrese el segundo número: ")) 
+# Suma de dos números
+primer_numero = float (input ("Ingrese el primer número: ")) 
+segundo_numero = float (input ("Ingrese el segundo número: ")) 
 resultado = primer_numero + segundo_numero
 print (resultado) 
