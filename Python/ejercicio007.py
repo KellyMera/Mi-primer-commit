38abb130e9509c666089e6b43405329032cdb9fc
KellyMera <milenamera01@gmail.com>
2021-06-07 22:40:28 -0500

    Operadores matemáticos

---
+++ Python/ejercicio007.py
@@ -0,0 +1,21 @@
+#Ejercicio de operadores matemáticos
+num1 = 5
+num2 = 2
+
+#Operaciones
+exponente = 5 ** 2 #25
+suma = 5 + 2 #7
+resta = 5 - 2 #3
+multiplicacion = 5 * 2 #10
+division = 5 / 2 #2.5
+division_entero = 5 // 2 #2
+modulo = 5 % 2 #1
+
+#Impresiones
+print (exponente)
+print (suma)
+print (resta)
+print (multiplicacion)
+print (division)
+print (division_entero)
+print (modulo)
