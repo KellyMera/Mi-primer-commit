504ae3f8c5d35971dc860f1394e319c92b13733f
KellyMera <milenamera01@gmail.com>
2021-06-07 19:02:47 -0500

    Mayor y menor de edad

--- Python/ejercicio005.py
+++ Python/ejercicio005.py
@@ -1,7 +1,9 @@
 #Ejercicio mayor y menor de edad
 
 edad = int(input("Por favor, ingrese su edad: "))
-if edad >= 18:
+if edad > 64:
+    print ("Eres anciano")
+elif edad >= 18:
     print ("Usted es mayor de edad")
 else:
     print ("Usted es menor de edad")
